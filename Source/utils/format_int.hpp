e13e1d94d1c3dc186575b438d0bf969999bb4479
Eric Robinson <ericjrobinson@live.com>
2025-04-06 23:36:27 -0400

    FormatInteger (unsigned)

--- Source/utils/format_int.hpp
+++ Source/utils/format_int.hpp
@@ -8,5 +8,6 @@ namespace devilution {
  * @brief Formats integer with thousands separator.
  */
 std::string FormatInteger(int n);
+std::string FormatInteger(uint32_t n);
 
 } // namespace devilution
